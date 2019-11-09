24d1fa649ae5517c8e83672ef1a73cb2e0a3a26b
Jon Martin <gametime785@gmail.com>
2019-11-09 04:33:06 -0600

    Setting up Index page.

--- tagfinder/tagfind/views.py
+++ tagfinder/tagfind/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Website,Tag
 
 # Create your views here.
+
+def index(request):
+    return render(request,'tagfind/index.html')
--- tagfinder/tagfinder/settings.py
+++ tagfinder/tagfinder/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tagfinder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')], #Directs Django to the global templates folder.
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
