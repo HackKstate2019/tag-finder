acef2da09bc1c0d3fa5cc41a53612742ac85b994
Jon Martin <gametime785@gmail.com>
2019-11-09 14:38:31 -0600

    Set tagfind to the index page.

--- tagfinder/tagfinder/urls.py
+++ tagfinder/tagfinder/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('tagfind/',include('tagfind.urls')),
+    path('',include('tagfind.urls')),
     path('admin/', admin.site.urls),
-    
 ]
