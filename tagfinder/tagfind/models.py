8ba3fced4d330783554e1d486b0a28c949bc4cb6
Jon Martin <gametime785@gmail.com>
2019-11-11 11:10:36 -0600

    Cleaning up and clarifying.

--- tagfinder/tagfind/models.py
+++ tagfinder/tagfind/models.py
@@ -7,7 +7,7 @@ class Website(models.Model):
     title = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.title
+        return self.url
 
 class Tag(models.Model):
     website = models.ForeignKey(Website,on_delete=models.CASCADE)
