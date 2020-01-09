5dd1140580239c2305e521bd1e6b4ae3cffb245d
Jon Martin <gametime785@gmail.com>
2020-01-09 17:38:56 -0600

    Cleaning up comments.

--- tagfinder/tagfind/views.py
+++ tagfinder/tagfind/views.py
@@ -25,12 +25,10 @@ def get_url(request):
             exists_model_parser(t) #Checks if Article is already in DB, if not then it makes a new Website and appends Tags to it.
             c=compare(t) #Compares Input Article to all of the Articles in the DB, forming Reference Ratings (RR) between each page.
 
-            print('Compare\'s Output: ', c)
-            print('Compare\'s [0]: ', c[0])
-            print('Compare\'s [0][0]: ', c[0][0])
-            print('Compare\'s Output Length: ', len(c))
-
-            # return render(request, 'tagfind/landing.html', {'form':form})
+            # print('Compare\'s Output: ', c)
+            # print('Compare\'s [0]: ', c[0])
+            # print('Compare\'s [0][0]: ', c[0][0])
+            # print('Compare\'s Output Length: ', len(c))
 
             #This is expecting the actual information to just be passed on.
             titlevar1=c[0][1]
@@ -44,25 +42,6 @@ def get_url(request):
             titlevar3=c[2][1]
             siteurl3=c[2][0]
             rr3=c[2][2]
-
-            #This is expecting the website's ID, not the actual information.
-            # titlevar1=Website.objects.filter(id=int(c[0][0])).values('title')
-            # siteurl1=Website.objects.filter(id=int(c[0][0])).values('url')
-            # rr1=str(c[0][1])
-            # try:
-            #     titlevar2=Website.objects.filter(id=int(c[1][0])).values('title')
-            #     siteurl2=Website.objects.filter(id=int(c[1][0])).values('url')
-            #     rr2=str(c[1][1])
-            #     titlevar3=Website.objects.filter(id=int(c[2][0])).values('title')
-            #     siteurl3=Website.objects.filter(id=int(c[2][0])).values('url')
-            #     rr3=str(c[2][1])
-            # except:
-            #     titlevar2=''
-            #     siteurl2=''
-            #     rr2=''
-            #     titlevar3=''
-            #     siteurl3=''
-            #     rr3=''
 
             searchedtitle=t[0][1]
             searchedurl=t[0][0]
